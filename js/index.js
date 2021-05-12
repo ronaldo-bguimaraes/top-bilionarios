94790991b9028e0ec028a392f7f32d12da966e33
Ronaldo Borges Guimarães <ronaldo.bguimaraes@outlook.com>
2021-05-12 00:19:10 -0400

    remove ajax jquery

--- js/index.js
+++ js/index.js
@@ -1,35 +1,26 @@
-const news = $("#news");
+const url = "https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=99";
 
-$.ajax({
+function sucessJson(json) {
 
-  type: "GET",
+  for (const item of json.items) {
 
-  url: "https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=99",
+    const imagens = JSON.parse(item.imagens);
 
-  dataType: "json",
+    const card_img = `<img class="card-img" src="https://ibge.gov.br/${imagens.image_intro}">`;
 
-  success: function (json) {
-
-    console.log(json);
-
-    for (const item of json.items) {
-
-      const imagens = JSON.parse(item.imagens);
-
-      const card_img = `<img class="card-img" src="https://ibge.gov.br/${imagens.image_intro}">`;
-
-      news.append(`
-        <div class="col-lg">
-          <div class="card my-3 text-justify">
-            ${imagens.image_intro ? card_img : ""}
-            <div class="card-body">
-              <h5 class="card-title">${item.titulo}</h5>
-              <p class="card-text">${item.introducao}</p>
-              <a href="${item.link}" target="_blank" class="btn btn-primary">Abrir em nova guia</a>
-            </div>
+    $("#news").append(`
+      <div class="col-lg">
+        <div class="card my-3 text-justify">
+          ${imagens.image_intro ? card_img : ""}
+          <div class="card-body">
+            <h5 class="card-title">${item.titulo}</h5>
+            <p class="card-text">${item.introducao}</p>
+            <a href="${item.link}" target="_blank" class="btn btn-primary">Abrir em nova guia</a>
           </div>
         </div>
-      `);
-    }
+      </div>
+    `);
   }
-});
+}
+
+fetch(url).then(e => e.json()).then(sucessJson);
